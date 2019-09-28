735c2b38ae3d6355e6d8c1d55c2fc82689366da1
Charles Jeffries <sazrocks6@gmail.com>
2019-09-28 12:56:22 -0700

    end of lab 9-27-19

--- 9-27-19_Drive_Straight/firstAttemptToDriveStraight.m
+++ 9-27-19_Drive_Straight/firstAttemptToDriveStraight.m
@@ -1,8 +1,15 @@
 speed = 100;
-time = 5;
+%time = 5;
 
+%100 - 5
+%80  - 5
+%60  - 5
+%40  - 8
+%20  - 18
 %Reset the absolute position of both motors to 0.
 brick.ResetMotorAngle('AB');
-brick.MoveMotor('AB', speed);
-pause(time);
-brick.StopAllMotors('Brake');
+brick.MoveMotorAngleRel('A', speed, 2200, 'Brake');
+brick.MoveMotorAngleRel('B', speed, 2200, 'Brake');
+
+%pause(time);
+%brick.StopAllMotors('Brake');
