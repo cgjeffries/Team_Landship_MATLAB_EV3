3bd71a3dd78837e6274badd7de0dfe55532205e7
Charles Jeffries <sazrocks6@gmail.com>
2019-10-18 10:48:43 -0700

    initial maze code

---
+++ Maze code/Maze.m
@@ -0,0 +1,16 @@
+
+brick.SetColorMode(1,2);
+const int RED = 5;
+const int YELLOW = 4;
+const int GREEN = 3;
+
+while true
+    switch brick.ColorCode(1)
+        case RED
+            
+            break;
+            
+        
+    end
+    
+end
