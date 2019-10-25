eaf7ae00ebef6eb8852bfff918e1fe811380fe28
ZeSquared <55895286+ZeSquared@users.noreply.github.com>
2019-10-25 14:00:37 -0700

    Made into a function, added esc case

--- Maze code/KeyboardControl.m
+++ Maze code/KeyboardControl.m
@@ -1,44 +1,48 @@
-global key
-InitKeyboard();
-controlActive = true;
+function Manual() 
+    global key
+    InitKeyboard();
+    controlActive = true;
 
-global MOTOR_DRIVE;
-global MOTOR_SHIFT;
-global MOTOR_LIFT;
-global brick;
-global liftTopPos;
-global liftBottomPos;
+    global MOTOR_DRIVE;
+    global MOTOR_SHIFT;
+    global MOTOR_LIFT;
+    global brick;
+    global liftTopPos;
+    global liftBottomPos;
 
-while controlActive
-    pause(0.05);
-    switch key
-        case 'uparrow'
-            Shift("straight");
-            brick.MoveMotor(MOTOR_DRIVE, 100);
-            
-        case 'downarrow'
-            Shift("straight");
-            brick.MoveMotor(MOTOR_DRIVE, -100);
-            
-        case 'leftarrow'
-            Shift("turn");
-            brick.MoveMotor(MOTOR_DRIVE, -100);
-            
-        case 'rightarrow'
-            Shift("turn");
-            brick.MoveMotor(MOTOR_DRIVE, 100);
-            
-        case 'w'
-            brick.MoveMotor(MOTOR_LIFT, 100);
-            
-        case 's'
-            brick.MoveMotor(MOTOR_LIFT, -100);
-            
-        otherwise
-            brick.StopMotor(MOTOR_DRIVE, 'Brake');
-            brick.StopMotor(MOTOR_LIFT, 'Brake');
-            
-            
+    while controlActive
+        pause(0.05);
+        switch key
+            case 'uparrow'
+                Shift("straight");
+                brick.MoveMotor(MOTOR_DRIVE, 100);
+
+            case 'downarrow'
+                Shift("straight");
+                brick.MoveMotor(MOTOR_DRIVE, -100);
+
+            case 'leftarrow'
+                Shift("turn");
+                brick.MoveMotor(MOTOR_DRIVE, -100);
+
+            case 'rightarrow'
+                Shift("turn");
+                brick.MoveMotor(MOTOR_DRIVE, 100);
+
+            case 'w'
+                brick.MoveMotor(MOTOR_LIFT, 100);
+
+            case 's'
+                brick.MoveMotor(MOTOR_LIFT, -100);
+            case 'esc'
+                controlActive = false;
+
+            otherwise
+                brick.StopMotor(MOTOR_DRIVE, 'Brake');
+                brick.StopMotor(MOTOR_LIFT, 'Brake');
+
+
+        end
     end
+    CloseKeyboard();
 end
-CloseKeyboard();
