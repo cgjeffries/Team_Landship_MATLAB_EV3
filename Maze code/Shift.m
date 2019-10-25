e89cda94b2e04ceabd86de31569c2e1cb9e0cced
Charles Jeffries <sazrocks6@gmail.com>
2019-10-25 12:24:56 -0700

    commit

--- Maze code/Shift.m
+++ Maze code/Shift.m
@@ -1,10 +1,20 @@
 function Shift(mode)
+    global MOTOR_DRIVE;
+    global MOTOR_SHIFT;
+    global MOTOR_LIFT;
+    global brick;
+    
+    global shiftTurnPos;
+    global shiftStraightPos;
     SHIFT_SPEED = 20;
+    %MOTOR_SHIFT = 'B';
     if mode == "straight"
+        disp MOTOR_SHIFT
         brick.MoveMotorAngleAbs(MOTOR_SHIFT, SHIFT_SPEED, shiftStraightPos);
-        brick.waitForMotor(MOTOR_SHIFT);
+        brick.WaitForMotor(MOTOR_SHIFT);
     elseif mode == "turn"
         brick.MoveMotorAngleAbs(MOTOR_SHIFT, SHIFT_SPEED, shiftTurnPos);
-        brick.waitForMotor(MOTOR_SHIFT); 
+        brick.WaitForMotor(MOTOR_SHIFT); 
     end
 end
+
