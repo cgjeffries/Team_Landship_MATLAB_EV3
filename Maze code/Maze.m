83ff5c9162a00bd1884b05c154514b2d24fbe53a
Layla255 <55896427+Layla255@users.noreply.github.com>
2019-10-25 19:57:20 -0700

    Update Maze.m

--- Maze code/Maze.m
+++ Maze code/Maze.m
@@ -44,13 +44,21 @@ while true
     color = brick.ColorCode(COLOR_SENSOR_PORT);
     switch color
         case RED
-            
+              %stop for two seconds
+              %go forward 2 inches
+
             break;
         case GREEN
-            
+             %enter manual control mode = person picking
+             %wait for manual control mode to finish
+             %set pick up flag (a var) to true
+             %drive forward 6 inches
+             %go back to checking if ground if red, green, yellow
+
             break;
         case YELLOW
-            
+            %if (personPickedUp) drive forward ten inches, lower fork, and drop passenger - - then exit
+            % else - - determine if distance
             break;
         otherwise
             if getDistance() > RIGHT_GAP_THRESHOLD
