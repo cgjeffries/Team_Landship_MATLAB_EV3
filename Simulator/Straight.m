b4516920415711cfe331d0348003ad3820dccc61
Charles Jeffries <sazrocks6@gmail.com>
2019-11-01 12:37:22 -0700

    clean up

--- Simulator/Straight.m
+++
@@ -1,8 +0,0 @@
-%Made by Zoe
-%input is a float in inches
-function Straight(x)
-    SPEED = 80;
-    angle = (x/6.8) * 360;
-    Shift("straight");
-    brick.MoveMotorAngleRel(MOTOR_DRIVE, SPEED, angle);
-end
